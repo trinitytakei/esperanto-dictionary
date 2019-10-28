c2f86f2c86b112f7b00b8871d4461013f5aa2182
Trinity Takei <trinity.takei@gmail.com>
2019-10-28 09:24:43 +0100

    Add the language swapper SVG; Start using dictionary files rather than hardcoded arrays

--- src/components/OriginalWords.vue
+++ src/components/OriginalWords.vue
@@ -2,11 +2,18 @@
   <section id="top-wrapper" class="flex-vertical-center">
     <div id="languages-wrapper">
       <span id="from-language">English</span>
-      <!-- <img viewBox="0 0 60 55" id="swap-languages" src="./src/assets/icons/exchange-arrows.svg" /> -->
+      <language-swapper id="swap-languages"></language-swapper>
       <span id="to-language">Esperanto</span>
     </div>
     <div class="textbox-container">
-      <input class="word-to-translate" type="text" name="word-to-translate"  v-model="inputWord" @keyup="translateWord">
+      <input
+        class="word-to-translate"
+        type="text"
+        name="word-to-translate"
+        v-model="inputWord"
+        @keyup="translateWord"
+        @focus="inputWord = ''"
+      />
       <span class="word-to-translate"></span>
     </div>
   </section>
@@ -14,39 +21,46 @@
 
 <script>
 import eventBus from "../eventBus.js";
+import dictionary from "../esperanto_english_dict.js";
+import LanguageSwapper from "./LanguageSwapper";
 
 export default {
+  components: {
+    LanguageSwapper
+  },
   data: function() {
     return {
       inputWord: "Enter word here",
-      translation: null,
-      dictionary: {
-        red: "rojo",
-        green: "verde",
-        blue: "azul",
-        yellow: "amarillo",
-        gray: "gris",
-        black: "negro",
-        pink: "rosa",
-        white: "blanco",
-        gold: "dorado"
-      }
+      exact_translations: null,
+      related_translations: null,
+      dictionary: dictionary
     };
   },
   methods: {
     translateWord() {
       if (this.inputWord.length <= 1) {
-        this.translation = "";
+        this.exact_translations = "";
       } else {
-        this.translation = this.dictionary[this.inputWord];
+        this.exact_translations = dictionary
+          .filter(pair => pair[0] == this.inputWord.toLowerCase())
+          .map(pair => pair[1]);
+        this.related_translations = dictionary
+          .filter(
+            pair =>
+              pair[0] !== this.inputWord.toLowerCase() &&
+              pair[0].includes(this.inputWord.toLowerCase())
+          )
+          .map(pair => [pair[0], pair[1]]);
       }
 
-      eventBus.$emit("translationReady", this.translation);
+      eventBus.$emit("translationReady", [
+        this.exact_translations.slice(0, 50),
+        this.related_translations.slice(0, 50),
+        this.inputWord
+      ]);
     }
   }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
