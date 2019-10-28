9cba4427965ec5be5a1027d2fe8171e7bc2d2dee
Trinity Takei <trinity.takei@gmail.com>
2019-10-28 09:23:29 +0100

    Add dictionary files - espdic_small and esperanto_english_dict.js are small files for testing, the real dictionary is in espdic.js / espdic.txt

---
+++ src/esperanto_english_dict.js
@@ -0,0 +1,13 @@
+// dict = open('espdic_small.txt').readlines
+// json_dict = dict.map{|e| e.chomp.split(' : ')}.to_json.gsub("],","],\n")
+// open('esperanto_english_dict.js','w'){|f| f.puts json_dict}
+
+const esperanto_english_dict = [
+  ["Aarono","Aaron"],
+  ["Abadono","Abaddon (biblical, a compartment of Gehenna)"],
+  ["abaĵuro","lampshade"],
+  ["abako","abacus, ball frame, counting frame, calculating frame, chart"],
+  ["abakteria","abacterial"]
+];
+
+export default esperanto_english_dict.map( pair => pair.map(s => s.toLowerCase() ) )
