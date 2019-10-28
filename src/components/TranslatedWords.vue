320a02d47c918211875caaf28b58560a4995f30f
Trinity Takei <trinity.takei@gmail.com>
2019-10-28 09:26:02 +0100

    Display translations (including nothing entered yet, no match found) ; Delete placeholder text when entering the text field for tranlations

--- src/components/TranslatedWords.vue
+++ src/components/TranslatedWords.vue
@@ -1,26 +1,45 @@
 <template>
-    <section id="bottom-wrapper">
-      <div class="results-container">
-
-        <div class="results-header">Translations of <strong>walnut</strong></div>
-
-        <div class="results-table">
-
-          <div class="results-table-cell translation"><strong>something super long</strong></div>
-          <div class="results-table-cell meaning">{{ this.translatedWords }}</div>
-
-          <div class="results-table-cell translation"><strong>nuez</strong></div>
-          <div class="results-table-cell meaning">a blaack fox jumps over a big fox and this is something super long and this is something super long and this is something super long and this is something super long and this is something super long</div>
-
-          <div class="results-table-cell translation"><strong>nuez</strong></div>
-          <div class="results-table-cell meaning">a blaack fox jumps over a big fox</div>
-
-          <div class="results-table-cell translation"><strong>nuez</strong></div>
-          <div class="results-table-cell meaning">a blaack fox jumps over a big fox</div>
-        </div>
+  <section id="bottom-wrapper">
+    <div class="results-container">
+      <div class="results-header">
+        Translations
+        <span v-if="word_to_translate !== ''">
+          of <strong>{{ word_to_translate }}</strong>
+        </span>
       </div>
 
-    </section>
+      <div class="results-table">
+        <div class="results-table-cell translation">
+          <span v-if="word_to_translate === ''">
+            Your search results will appear here.
+          </span>
+          <span v-else>
+            <span v-if="this.exact_translations.length > 0">
+              <strong>{{ word_to_translate }}</strong>
+            </span>
+            <span v-else>
+              No Matches found!
+            </span>
+          </span>
+        </div>
+        <div
+          class="results-table-cell meaning"
+          v-for="tr in this.exact_translations"
+          :key="tr"
+        >
+          {{ tr }}
+        </div>
+
+        <div
+          class="results-table-cell meaning"
+          v-for="tr in this.related_translations"
+          :key="tr"
+        >
+          {{ tr }}
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -29,17 +48,21 @@ import eventBus from "../eventBus.js";
 export default {
   data: function() {
     return {
-      translatedWords: ""
+      word_to_translate: "",
+      exact_translations: "",
+      related_translations: ""
     };
   },
   created() {
-    eventBus.$on("translationReady", translatedWords => {
-      this.translatedWords = translatedWords;
+    eventBus.$on("translationReady", translation_data => {
+      [
+        this.exact_translations,
+        this.related_translations,
+        this.word_to_translate
+      ] = translation_data;
     });
   }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
